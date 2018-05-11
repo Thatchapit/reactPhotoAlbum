52d5571a1c821f5c871b435d6e93c97895144371
Thatchapit <lnwgee@gmail.com>
2018-05-11 18:32:56 +0700

    react

---
+++ src/pages/User.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react'
+
+class User extends Component{
+    render(){
+        return(
+            <div>
+                Page User for photo
+            </div>
+        )
+    }
+}
+
+export default User
